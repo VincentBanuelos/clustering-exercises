8e3d95536644d5655cd9ccdb01bb3fa5a0ecfad5
Vincent Banuelos <vincent.v.banuelos527@gmail.com>
2022-09-21 10:49:43 -0500

    adding clustering exercises and supporting python files

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -78,12 +78,49 @@ def nulls_by_col(df):
 
 
 def prep_zillow(df):
+    # Changing bedrooms to a discrete variable
+    df.bedroomcnt = df.bedroomcnt.astype(object)
+    df = df[df.bedroomcnt > 0.0]
+
+    # Changing bedrooms to a discrete variable
+    df.bathroomcnt = df.bathroomcnt.astype(object)
+    df = df[df.bathroomcnt > 0.0]
+
+    #Changing zipcodes to discrete values
+    df.regionidzip = df.regionidzip.astype(object)
     # Dropping rows within the dataframe that are either null or have a lot size of 0.
     df.lotsizesquarefeet = df.lotsizesquarefeet.fillna(0)
-    df.lotsizesquarefeet = df.lotsizesquarefeet != 0
+    df = df[df.lotsizesquarefeet != 0.0]
 
     # Dropping duplexes and triplexes
     df.unitcnt = df.unitcnt = 1
+
+    # filtering for only transactions in 2017
+    df = df[df.transactiondate < '2018-01-01']
+
+    # Dropping nulls in sqft
+    df = df[df['calculatedfinishedsquarefeet'].notna()]
+
+    # create column with fips value converted from an integer to the county name string
+    df['fips'] = df.fips.map({6037.0 : 'los_angeles', 6059.0 : 'orange', 6111.0 : 'ventura'})
+    
+    # Making sure properties are not tax deliquent
+    df.taxdelinquencyflag = df.taxdelinquencyflag = 'Y'
+
+    # rename columns for clarity
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 'bathroomcnt':'bathrooms', \
+        'calculatedfinishedsquarefeet':'sqft','taxvaluedollarcnt':'tax_value', \
+        'lotsizesquarefeet':'lotsize', \
+        'fips':'county','regionidzip':'zip'})
+
+    # one-hot encode county
+    dummies = pd.get_dummies(df['county'],drop_first=False,dtype=float)
+    df = pd.concat([df, dummies], axis=1)
+
+    #dropping columns due to having repeated values
+    df = df.drop(columns=['roomcnt','unitcnt','propertylandusedesc','parcelid','id','finishedsquarefeet12','rawcensustractandblock','structuretaxvaluedollarcnt','landtaxvaluedollarcnt'])
+
+    df = df.astype({'bathrooms':'object','bedrooms':'object','calculatedbathnbr':'object','county':'object','fullbathcnt':'object','propertycountylandusecode':'object','propertylandusetypeid':'object','regionidcity':'object','regionidcounty':'object','censustractandblock':'object'})
 
     return df
 
@@ -105,3 +142,13 @@ def handle_missing_values(df, prop_required_column, prop_required_row):
     df.dropna(axis=0, thresh=threshold, inplace=True)
     
     return df
+
+def wrangle_zillow():
+    df = get_zillow()
+    df = prep_zillow(df)
+    df = handle_missing_values(df, .75, .75)
+
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+
+    return train, validate, test
